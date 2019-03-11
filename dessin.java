49b2f282a4881c6471a2f5af58e830c8417a56d4
JulieBerr <47639018+JulieBerr@users.noreply.github.com>
2019-03-11 17:50:20 +0100

    dessin

--- dessin.java
+++ dessin.java
@@ -5,22 +5,41 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
+		public String forme="";
+		
+		final ImageButton blanc = findViewById(R.id.blanc);
+		blanc.setOnClickListener(new View.OnClickListener(){
+			public void onClick(View v){
+				switch (forme) {
+					case "carre":
+						draw(R.carre);
+						break;
+					case "cercle":
+						draw(R.cercle);
+						break;
+					case "triangle":
+						draw(R.triangle);
+						break;
+				}
+				
+			}		
+		
 		final ImageButton carre = findViewById(R.id.carre);
 		carre.setOnClickListener(new View.OnClickListener(){
 			public void onClick(View v){
-				draw(R.carre);
+				forme="carre";
 			}
 			
 		final ImageButton cercle = findViewById(R.id.cercle);
 		carre.setOnClickListener(new View.OnClickListener(){
 			public void onClick(View v){
-				draw(R.cercle);
+				forme="cercle";
 			}
 			
 		final ImageButton triangle = findViewById(R.id.triangle);
 		carre.setOnClickListener(new View.OnClickListener(){
 			public void onClick(View v){
-				playSound(R.triangle);
+				forme="triangle";
 			}
 		}
 	}
