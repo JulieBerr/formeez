0c136dadef6c7b6c41d7e06e0919f61dacf7459d
JulieBerr <47639018+JulieBerr@users.noreply.github.com>
2019-03-16 11:33:16 +0100

    pour la cohésion

--- quizz.java
+++ quizz.java
@@ -1,11 +1,3 @@
-//*******************************************************************
-// Welcome to CompileJava!
-// If you experience any issues, please contact us ('More Info')  -->
-// Also, sorry that the "Paste" feature no longer works! GitHub broke
-// this (so we'll switch to a new provider): https://blog.github.com\
-// /2018-02-18-deprecation-notice-removing-anonymous-gist-creation/
-//*******************************************************************
-
 import java.lang.Math; // headers MUST be above the first class
 
 // one class needs to have a main() method
