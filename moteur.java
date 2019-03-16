dbd938caba267c5985f52a5a106c6d9ff9953522
JulieBerr <47639018+JulieBerr@users.noreply.github.com>
2019-03-16 11:32:31 +0100

    pour la cohésion

--- moteur.java
+++ moteur.java
@@ -1,14 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package CarreTriangleRond;
 
-/**
- *
- * @author gwandelle
- */
 //Le moteur choisis entre carré, triangle et rond
 public class moteur {
     /**
