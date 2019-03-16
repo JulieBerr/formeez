1531e8b1ef9609ca2a7f7a5124c2f92df5b989de
JulieBerr <47639018+JulieBerr@users.noreply.github.com>
2019-03-16 11:31:03 +0100

    pour la cohésion

--- Triangle.java
+++ Triangle.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package CarreTriangleRond;
 
-/**
- *
- * @author gwandelle
- */
 public class Triangle extends Forme{
         
     private int x;
