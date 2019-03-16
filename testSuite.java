8a2b34f0d779c7c339bf0577d41ff1a49dc32b4b
JulieBerr <47639018+JulieBerr@users.noreply.github.com>
2019-03-16 10:51:46 +0100

    tests

---
+++ testSuite.java
@@ -0,0 +1,11 @@
+import com.google.common.truth.Truth.assertThat;
+import org.junit.Test;
+
+public class test {
+    @Test
+    public void test {
+        assertThat(onCreate()).isTrue();
+    }
+	
+	
+}
