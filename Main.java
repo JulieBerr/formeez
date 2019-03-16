ca24656f4de49e038865313bf8aceda5868b5e3e
JulieBerr <47639018+JulieBerr@users.noreply.github.com>
2019-03-16 11:29:58 +0100

    pour la cohésion

--- Main.java
+++ Main.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package CarreTriangleRond;
 
-/**
- *
- * @author gwandelle
- */
 public class Main {
     public static void main(String[] args) {
         Cercle c=new Cercle(10, 0,0);
