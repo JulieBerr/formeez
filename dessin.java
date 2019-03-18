ff2827bf771de5c9eaedba8e41d17fb5f1e0e99a
JulieBerr <47639018+JulieBerr@users.noreply.github.com>
2019-03-18 18:14:32 +0100

    ajout du dessin

--- dessin.java
+++ dessin.java
@@ -13,18 +13,61 @@ public class MainActivity extends Activity {
 			public void onClick(View v){
 				switch (forme) {
 					case "carre":
-						draw(R.carre);
+						DessinerRectangle(context);
 						break;
 					case "cercle":
-						draw(R.cercle);
+						DessinerOval(context);
 						break;
 					case "triangle":
-						draw(R.triangle);
+						DessinerTriangle(context);
 						break;
 				}
 				
 			}		
-		
+		public void DessinerOval(Context context){
+		super(context);
+
+    		int x = 10;
+    		int y = 10;
+    		int width = 300;
+    		int height = 50;
+
+    		drawable = new ShapeDrawable(new OvalShape());
+    // If the color isn't set, the shape uses black as the default.
+   		drawable.getPaint().setColor(0xff74AC23);
+    // If the bounds aren't set, the shape can't be drawn.
+    		mDrawable.setBounds(x, y, x + width, y + height);
+  
+	}
+	
+	public void DessinerRectangle(Context context){
+		 super(context);
+
+   		int x = 10;
+   		int y = 10;
+    		int width = 300;
+    		int height = 50;
+
+    drawable = new ShapeDrawable(new RectShape());
+    // If the color isn't set, the shape uses black as the default.
+    drawable.getPaint().setColor(0xff74AC23);
+    // If the bounds aren't set, the shape can't be drawn.
+    mDrawable.setBounds(x, y, x + width, y + height);
+  }
+	}
+
+	public void DessinerTriangle(Context context){
+		Paint p = new Paint();
+		p.setStyle(Paint.Style.FILL);
+
+		float[] pts = ((0, 0), (0, 100), (50, 50));
+
+		drawPoints(float[] pts, p);
+	}
+	
+  protected void onDraw(Canvas canvas) {
+    mDrawable.draw(canvas);
+  }
 		final ImageButton carre = findViewById(R.id.carre);
 		carre.setOnClickListener(new View.OnClickListener(){
 			public void onClick(View v){
