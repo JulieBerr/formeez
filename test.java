562e799ba546042c3f60769c003452cccd62216b
JulieBerr <47639018+JulieBerr@users.noreply.github.com>
2019-03-18 18:07:57 +0100

    tests de l'accueil

--- test.java
+++ test.java
@@ -4,7 +4,14 @@ import org.junit.Test;
 public class test {
     @Test
     public void test {
-        assertThat(onCreate()).isTrue();
+	    Bundle bundle=null;
+        assertThat(Accueil.onCreate(bundle)).isTrue();
+	    ListView parent=null;
+	    View v=null;
+	    int position=0;
+	    long id=0;
+	assertThat(Accueil.onListItemClick(parent, v, int position, long id).isTrue();
+	    
     }
 	
 	
