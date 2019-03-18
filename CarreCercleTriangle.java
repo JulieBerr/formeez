71db3077198d5f22e1611ca5519bc825f8fdd233
JulieBerr <47639018+JulieBerr@users.noreply.github.com>
2019-03-18 17:46:08 +0100

    Ajout de la fonction dessiner oval

--- CarreCercleTriangle.java
+++ CarreCercleTriangle.java
@@ -1,5 +1,6 @@
-public classe CarreCercleTriangle{
-
+public class CarreCercleTriangle extends View{
+private ShapeDrawable drawable;
+	
 	public void forme(){
  
         	H h = new H(0,2,1);
@@ -25,6 +26,23 @@ public classe CarreCercleTriangle{
         
      }
 
-	
+	public void DessinerOval(){
+		super(context);
 
+    		int x = 10;
+    		int y = 10;
+    		int width = 300;
+    		int height = 50;
+
+    		drawable = new ShapeDrawable(new OvalShape());
+    // If the color isn't set, the shape uses black as the default.
+   		drawable.getPaint().setColor(0xff74AC23);
+    // If the bounds aren't set, the shape can't be drawn.
+    		mDrawable.setBounds(x, y, x + width, y + height);
+  
+	}
+	
+  protected void onDraw(Canvas canvas) {
+    mDrawable.draw(canvas);
+  }
 }
