bd57eec8aeda8d6bb7f5f4cf3d64b1817cefc6ba
JulieBerr <47639018+JulieBerr@users.noreply.github.com>
2019-03-18 17:49:44 +0100

    Rectangle et Triangle

--- CarreCercleTriangle.java
+++ CarreCercleTriangle.java
@@ -26,7 +26,7 @@ private ShapeDrawable drawable;
         
      }
 
-	public void DessinerOval(){
+	public void DessinerOval(Context context){
 		super(context);
 
     		int x = 10;
@@ -40,6 +40,31 @@ private ShapeDrawable drawable;
     // If the bounds aren't set, the shape can't be drawn.
     		mDrawable.setBounds(x, y, x + width, y + height);
   
+	}
+	
+	public void DessinerRectangle(Context context){
+		 super(context);
+
+   		int x = 10;
+   		int y = 10;
+    		int width = 300;
+    		int height = 50;
+
+    drawable = new ShapeDrawable(new RectShape());
+    // If the color isn't set, the shape uses black as the default.
+    drawable.getPaint().setColor(0xff74AC23);
+    // If the bounds aren't set, the shape can't be drawn.
+    mDrawable.setBounds(x, y, x + width, y + height);
+  }
+	}
+
+	public void DessinerTriangle(Context context){
+		Paint p = new Paint();
+		p.setStyle(Paint.Style.FILL);
+
+		float[] pts = ((0, 0), (0, 100), (50, 50));
+
+		drawPoints(float[] pts, p);
 	}
 	
   protected void onDraw(Canvas canvas) {
